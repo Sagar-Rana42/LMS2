30837ed47b42053e868c30ca23e32ebecffecd7e
Sagar-Rana42 <146703452+Sagar-Rana42@users.noreply.github.com>
2025-07-25 00:21:42 +0530

    vite.config.js changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,4 @@
+/*
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
@@ -14,4 +15,21 @@ export default defineConfig({
       '/api':'http://localhost:4000'
     }
   }
+})
+*/
+
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'dist', 
+  },
 })
